06ecaae8bfeb95f238c022a3a1ddcd24d979c76c
PluginsKers <ikers@foxmail.com>
2024-02-25 14:12:15 +0000

    remove: app.config.from_mapping(SECRET_KEY="super_secret_key")

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ load_dotenv()
 def create_app() -> Flask:
     app = Flask(__name__, static_url_path="/")
     app.url_map.strict_slashes = False
-    app.config.from_mapping(SECRET_KEY="super_secret_key")
 
     # 注册蓝图
     app.register_blueprint(api_blueprint, url_prefix="/api")
