437466021711fb266d2d4edb69040878d9f9dbdc
PluginsKers <ikers@foxmail.com>
2024-04-02 19:34:47 +0000

    Delete irrelevant files left over from the merge.

--- src/views/routes/__init__.py
+++
@@ -1,5 +0,0 @@
-from src.views.routes.editor_routes import *
-from src.views.routes.search_routes import *
-from src.views.routes.wecom_routes import *
-from src.views.routes.user_routes import *
-from src.views.routes.auth_routes import *
